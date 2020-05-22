2463e27bfb23d8a5c5e3dd7e19ea4b00d4567d37
Yen-Shi Wang <yenshiw@gmail.com>
2020-05-21 22:51:20 -0400

    Update to v0.7

--- travian.js
+++ travian.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TravianBot
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  A script for auto-constructing farms and buildings
 // @author       Yen-Shi Wang
 // @match        https://ts3.travian.com/dorf1.php*
@@ -14,17 +14,17 @@
 let server = 'ts3';
 
 let farmInfo = [
-  ['crop1', 4, 'buildingSlot2'],
-  ['crop2', 4, 'buildingSlot8'],
+  ['crop1', 5, 'buildingSlot2'],
+  ['crop2', 5, 'buildingSlot8'],
   ['crop3', 4, 'buildingSlot9'],
   ['crop4', 4, 'buildingSlot12'],
   ['crop5', 4, 'buildingSlot13'],
   ['crop6', 4, 'buildingSlot15'],
 
-  ['clay1', 6, 'buildingSlot5'],
-  ['clay2', 6, 'buildingSlot6'],
+  ['clay1', 5, 'buildingSlot5'],
+  ['clay2', 5, 'buildingSlot6'],
   ['clay3', 5, 'buildingSlot16'],
-  ['clay4', 5, 'buildingSlot18'],
+  ['clay4', 4, 'buildingSlot18'],
 
   ['wood1', 5, 'buildingSlot1'],
   ['wood2', 5, 'buildingSlot3'],
@@ -32,7 +32,7 @@ let farmInfo = [
   ['wood4', 5, 'buildingSlot17'],
 
   ['iron1', 5, 'buildingSlot4'],
-  ['iron2', 4, 'buildingSlot7'],
+  ['iron2', 5, 'buildingSlot7'],
   ['iron3', 4, 'buildingSlot10'],
   ['iron4', 4, 'buildingSlot11'],
 ];
@@ -44,41 +44,43 @@ let farmInfo = [
 // 'tabItem resources'
 // 'tabItem military'
 let buildingInfo = [
-  ['Residence', 10, 'buildingSlot g25', 1, 'tabItem infrastructure'],
-  ['Marketplace', 8, 'buildingSlot g17', 1, 'tabItem infrastructure'],
+  ['Cranny', 10, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+
+  ['Warehouse', 5, 'buildingSlot g10', 1, 'tabItem infrastructure'],
+  ['Granary', 5, 'buildingSlot g11', 1, 'tabItem infrastructure'],
+
+  ['Residence', 0, 'buildingSlot g25', 1, 'tabItem infrastructure'],
+  ['Marketplace', 10, 'buildingSlot g17', 1, 'tabItem infrastructure'],
   ['Smithy', 3, 'buildingSlot g13', 2, 'tabItem military'],
-  ['2. Cranny', 10, 'buildingSlot g23', 1, 'tabItem infrastructure'],
 
   // Infrastructure
   ['Main Building', 5, 'buildingSlot g15', 1, 'tabItem infrastructure'],
-  ['Granary', 7, 'buildingSlot g11', 1, 'tabItem infrastructure'],
-  ['Warehouse', 7, 'buildingSlot g10', 1, 'tabItem infrastructure'],
-  ['Cranny', 10, 'buildingSlot g23', 1, 'tabItem infrastructure'],
 
-  ['3. Cranny', 3, 'buildingSlot g23', 1, 'tabItem infrastructure'],
-  ['4. Cranny', 3, 'buildingSlot g23', 1, 'tabItem infrastructure'],
-  ['5. Cranny', 3, 'buildingSlot g23', 1, 'tabItem infrastructure'],
-  ['6. Cranny', 3, 'buildingSlot g23', 1, 'tabItem infrastructure'],
-  ['7. Cranny', 3, 'buildingSlot g23', 1, 'tabItem infrastructure'],
-  ['8. Cranny', 3, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['2. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['3. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['4. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['5. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['6. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['7. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
+  ['8. Cranny', 0, 'buildingSlot g23', 1, 'tabItem infrastructure'],
 
 
   ['Embassy', 1, 'buildingSlot g18', 1, 'tabItem infrastructure'],
-  ['Palace', 1, 'buildingSlot g26', 1, 'tabItem infrastructure'],
+  ['Palace', 0, 'buildingSlot g26', 1, 'tabItem infrastructure'],
   ['Stonemason\'s Lodge', 0, 'buildingSlot g34', 1, 'tabItem infrastructure'],
   ['Treasury', 0, 'buildingSlot g27', 1, 'tabItem infrastructure'],
-  ['Town Hall', 0, 'buildingSlot g24', 1, 'tabItem infrastructure'],
+  ['Town Hall', 10, 'buildingSlot g24', 1, 'tabItem infrastructure'],
   ['Trade Office', 0, 'buildingSlot g28', 1, 'tabItem infrastructure'],
   ['Great Granary', 0, 'buildingSlot g39', 1, 'tabItem infrastructure'],
   ['Great Warehouse', 0, 'buildingSlot g38', 1, 'tabItem infrastructure'],
 
   // Military
-  ['Rally Point', 3, 'buildingSlot g16', 2, 'tabItem military'],
-  ['Palisade', 1, 'buildingSlot g33', 2, 'tabItem military'],
+  ['Rally Point', 1, 'buildingSlot g16', 2, 'tabItem military'],
+  ['Palisade', 0, 'buildingSlot g33', 2, 'tabItem military'],
   ['Trapper', 0, 'buildingSlot g36', 2, 'tabItem military'],
   ['Barracks', 3, 'buildingSlot g19', 2, 'tabItem military'],
   ['Hero\'s Mansion', 0, 'buildingSlot g37', 2, 'tabItem military'],
-  ['Academy', 1, 'buildingSlot g22', 2, 'tabItem military'],
+  ['Academy', 3, 'buildingSlot g22', 2, 'tabItem military'],
 
   ['Stable', 0, 'buildingSlot g20', 2, 'tabItem military'],
   ['Workshop', 0, 'buildingSlot g21', 2, 'tabItem military'],
@@ -94,6 +96,13 @@ let buildingInfo = [
 
 let checkTime = 3320;
 
+let refreshPage = () => {
+  // every 10 mins
+  setTimeout(() => {
+    document.getElementsByClassName('buildingView')[0].click();
+  }, 600 * 1000);
+};
+
 /*
  * methods for building
  */
@@ -102,13 +111,12 @@ let getBuilding = (name) => {
 };
 
 let getBuildingLevel = (name, trueName) => {
-  let level = parseInt(getBuilding(name).children[0].children[0].innerText);
-  // console.log('Level of building', trueName, 'is', level);
+  let level = parseInt(getBuilding(name).children[0].innerText);
   return level;
 };
 
 let getLevelFromBuilding = (buliding) => {
-  let level = parseInt(buliding.children[0].children[0].innerText);
+  let level = parseInt(buliding.children[0].innerText);
   return level;
 };
 
@@ -369,4 +377,5 @@ let handleLogin = () => {
   } else if (url.match(/https:\/\/ts[0-9].travian.com\/login.php.*/)) {
     handleLogin();
   }
+  refreshPage();
 })();
